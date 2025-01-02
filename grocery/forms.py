2b09203b8c9223a233c6e52799d6e0c258faf295
ELVIS RONO <Elvis>
2025-01-02 20:12:44 +0300

    add bootstrap and templates

---
+++ grocery/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import TextInput, PasswordInput
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    #- login a user
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+
