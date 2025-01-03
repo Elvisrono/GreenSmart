f5a501ee07f4bd316a2679be7b6a0fc7da2b1094
ELVIS RONO <Elvis>
2025-01-03 20:51:41 +0300

    add host on settings

--- backend/settings.py
+++ backend/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xkd2twn$=#qn^7rzbwazoy(#d80ph=2fbm^el@3$8dylbr6#dh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1','.now.sh']
 
 
 # Application definition
